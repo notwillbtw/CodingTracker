50071af2c882b1b9e2cb8dd50c3eb84775a9ae64
notwillbtw <WillRowett@outlook.com>
2024-04-20 18:25:18 +0100

    Initial Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Academy! One more modification"); Console.WriteLine("Hello, C# Academy! One more modification"); Console.WriteLine("Hello, C# Academy! One more modification"); Console.WriteLine("Hello, C# Academy! One more modification"); Console.WriteLine("Hello, C# Academy! One more modification");
+﻿void MyFirstMethod()
+{
+    //do something
+}
+
+void MySecondMethod()
+{
+    //do something
+}
+
+void MyThirdMethod()
+{
+    //do something
+}
